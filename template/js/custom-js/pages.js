ca066ab6df2836a43a566fb29c3ca5fcf24ec7c9
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-12-23 19:12:42 -0300

    chore: layout medium

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
+const nameIf = window.ecomPassport.customer.display_name ? window.ecomPassport.customer.display_name : ' entre ou cadastre-se'
+document.getElementById('nameIf').innerHTML = 'Olá ' + nameIf
