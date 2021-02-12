1ee0024c5c55c2a33626e911e38a870af0aa4fe0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-02-12 11:40:17 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,3 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
 const nameIf = window.ecomPassport.customer.display_name ? window.ecomPassport.customer.display_name : ' entre ou cadastre-se'
 document.getElementById('nameIf').innerHTML = 'Olá ' + nameIf
+window.$ecomConfig.set('default_img_size', 'zoom')
