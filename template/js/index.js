ab5404e5a505e469b1733e312e761103ddee187f
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-12-23 18:06:46 -0300

    Update index.js

--- template/js/index.js
+++ template/js/index.js
@@ -1,2 +1,8 @@
 import '#template/js/'
 import './custom-js/pages'
+
+storefront.on('widget:@ecomplus/widget-tag-manager', function () {
+    setTimeout(function () {
+      $('#modal-size-guide').modal('show')
+    }, 800);
+});
