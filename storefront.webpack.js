826b8ddd65eb9283205c0ac234bd016976bc7d77
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-06-09 16:56:35 -0300

    build: updating with https://github.com/ecomplus/storefront-starter

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,9 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html')
+      
     }
   }
 })
