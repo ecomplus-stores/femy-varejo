89f54ef560adeef155ad43fb2f8be3afb0fb66d3
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-06-10 15:50:52 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+      //'./js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html')
       
     }
